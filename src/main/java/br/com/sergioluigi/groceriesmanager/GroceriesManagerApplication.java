c679b199ffa7055e040ecf88b4976e6a03ee0dc7
Sergio Luigi Alves <sergio.luigialves@hotmail.com>
2022-11-16 00:08:52 -0300

    First commit

---
+++ src/main/java/br/com/sergioluigi/groceriesmanager/GroceriesManagerApplication.java
@@ -0,0 +1,13 @@
+package br.com.sergioluigi.groceriesmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroceriesManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroceriesManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/sergioluigi/groceriesmanager/GroceriesManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sergioluigi.groceriesmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroceriesManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
