da671a9b690d1d41e84e0ccbb2c4fec21b2c10bb
sergioluigi <sergio.luigialves@hotmail.com>
2022-12-12 22:03:26 -0300

    Some other things

--- src/main/java/br/com/sergioluigi/groceriesmanager/infrastructure/database/adapter/product/FindProductByIdAdapter.java
+++ src/main/java/br/com/sergioluigi/groceriesmanager/infrastructure/database/adapter/product/FindProductByIdAdapter.java
@@ -1,7 +1,5 @@
 package br.com.sergioluigi.groceriesmanager.infrastructure.database.adapter.product;
 
-import br.com.sergioluigi.groceriesmanager.domain.exception.GroceriesManagerError;
-import br.com.sergioluigi.groceriesmanager.domain.exception.GroceriesManagerException;
 import br.com.sergioluigi.groceriesmanager.domain.model.Product;
 import br.com.sergioluigi.groceriesmanager.domain.usecase.product.ports.out.FindProductByIdOutPort;
 import br.com.sergioluigi.groceriesmanager.infrastructure.database.document.ProductDocument;
@@ -12,13 +10,9 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-import static br.com.sergioluigi.groceriesmanager.domain.exception.GroceriesManagerError.PRODUCT_NOT_FOUND;
-
 @Repository
 @AllArgsConstructor
 public class FindProductByIdAdapter implements FindProductByIdOutPort {
-
-    //private static final GroceriesManagerError PRODUCT_NOT_FOUND = ;
     private ProductRepository tagRepository;
 
     @Override
